e8e0fdc66204c2813ba93d583c4952006b5d9a09
Bjorn Backeberg <29396406+backeb@users.noreply.github.com>
2022-08-29 18:54:35 +0200

    started on library and function approach for cmems and era5

--- cmems/download_cmems_physics.py
+++ cmems/download_cmems_physics.py
@@ -1,75 +1,28 @@
 # coding: utf-8
-'''Purpose: download near real time hydrodynamics data from Copernicus Marine Service
+'''Purpose: download hydrodynamics data from Copernicus Marine Service
 
-Dependencies: `conda env create -f environment.yaml`
+Dependencies: 
+- register for username and password at <https://resources.marine.copernicus.eu/registration-form>.
+- `conda env create -f environment.yaml`
 
 Creation Date: 2 May 2022
 Author: backeb <bjorn.backeberg@deltares.nl> Bjorn Backeberg
 '''
-import click
 from datetime import timedelta, datetime
 from pathlib import Path
 import subprocess
 import xarray as xr # note dependencies: dask, netCDF4
 
-@click.command()
-@click.option(	'--username',
-		default='',
-		help='register at: https://resources.marine.copernicus.eu/registration-form')
-@click.option(	'--password',
-		default='',
-		help='register at: https://resources.marine.copernicus.eu/registration-form')
-@click.option(	'--longitude_min',
-		type=(float),
-		help='Minimum longitude for region of interest',
-		default=-180,
-		show_default=True)
-@click.option(	'--longitude_max',
-		type=(float),
-		help='Maximum longitude for region of interest',
-		default=180,
-		show_default=True)
-@click.option(	'--latitude_min',
-		type=(float),
-		help='Minimum latitude for region of interest',
-		default=-90,
-		show_default=True)
-@click.option(	'--latitude_max',
-		type=(float),
-		help='Maximum latitude for region of interest',
-		default=90,
-		show_default=True)
-@click.option(	'--date_min',
-		type=(str),
-		help='Start date for data download. Format: YYYY-MM-DD',
-		default=(datetime.now()).strftime('%Y-%m-%d'),
-		show_default=True)
-@click.option(	'--date_max',
-		type=(str),
-		help='End date for data download. Format: YYYY-MM-DD',
-		default=(datetime.now()).strftime('%Y-%m-%d'),
-		show_default=True)
-@click.option(	'--vars',
-		multiple=True,
-		help='available vars: https://catalogue.marine.copernicus.eu/documents/PUM/CMEMS-GLO-PUM-001-024.pdf',
-		default=(	'thetao',
-				'bottomT',
-				'so',
-				'zos',
-				'uo',
-				'vo'),
-		show_default=True)
-
-def runcommand(username, password, longitude_min, longitude_max, latitude_min, latitude_max, date_min, date_max, vars):
+def get_nrt_daily(username, password, longitude_min, longitude_max, latitude_min, latitude_max, date_min, date_max, vars, outdir):
 	#make the /data/tmp directory if it does not exist
-	Path('/data/cmems/tmp').mkdir(parents=True, exist_ok=True)
+	Path(outdir+'/cmems/tmp').mkdir(parents=True, exist_ok=True)
 	delta = datetime.strptime(date_max, '%Y-%m-%d') - datetime.strptime(date_min, '%Y-%m-%d')
 	for var in vars:
 		for i in range(delta.days+1):
 			max_runs = 2
 			run = 0
 			day = datetime.strptime(date_min, '%Y-%m-%d').date() + timedelta(days=i)
-			check_file = Path('/data/cmems/tmp/cmems_'+str(var)+'_'+str(day)+'.nc')
+			check_file = Path(outdir+'/cmems/tmp/cmems_'+str(var)+'_'+str(day)+'.nc')
 			while not check_file.is_file():
 				while run < max_runs:
 					try:
@@ -86,7 +39,7 @@ def runcommand(username, password, longitude_min, longitude_max, latitude_min, l
 							'--depth-min', '0.493',
 							'--depth-max', '5727.918000000001',
 							'--variable', str(var),
-							'--out-dir', '/data/cmems/tmp',
+							'--out-dir', outdir+'/cmems/tmp',
 							'--out-name', 'cmems_'+str(var)+'_'+str(day)+'.nc',
 							'--user', username,
 							'--pwd', password],
@@ -101,8 +54,49 @@ def runcommand(username, password, longitude_min, longitude_max, latitude_min, l
 						break
 					finally:
 						run += 1
-		ds = xr.open_mfdataset('/data/cmems/tmp/cmems_'+var+'_*.nc', combine='by_coords', decode_times=False)
-		ds.to_netcdf('/data/cmems/cmems_'+var+'.nc')
+	ds = xr.open_mfdataset(outdir+'/cmems/tmp/cmems_*.nc', combine='by_coords', decode_times=False)
+	ds.to_netcdf(outdir+'/cmems/cmems.nc')
 
-if __name__ == '__main__':
-	runcommand()
+def get_nrt_hourly(username, password, longitude_min, longitude_max, latitude_min, latitude_max, date_min, date_max, vars, outdir):
+	#make the /data/tmp directory if it does not exist
+	Path(outdir+'/cmems/tmp').mkdir(parents=True, exist_ok=True)
+	delta = datetime.strptime(date_max, '%Y-%m-%d') - datetime.strptime(date_min, '%Y-%m-%d')
+	for var in vars:
+		for i in range(delta.days+1):
+			max_runs = 2
+			run = 0
+			day = datetime.strptime(date_min, '%Y-%m-%d').date() + timedelta(days=i)
+			check_file = Path(outdir+'/cmems/tmp/cmems_'+str(var)+'_'+str(day)+'.nc')
+			while not check_file.is_file():
+				while run < max_runs:
+					try:
+						subprocess.run(['python', '-m', 'motuclient',
+							'--motu', 'https://nrt.cmems-du.eu/motu-web/Motu',
+							'--service-id', 'GLOBAL_ANALYSIS_FORECAST_PHY_001_024-TDS',
+							'--product-id', 'global-analysis-forecast-phy-001-024-hourly-t-u-v-ssh',
+							'--longitude-min',str(longitude_min),
+							'--longitude-max', str(longitude_max),
+							'--latitude-min', str(latitude_min),
+							'--latitude-max', str(latitude_max),
+							'--date-min', str(day)+' 00:00:00',
+							'--date-max', str(day)+' 23:59:00',
+							'--depth-min', '0.493',
+							'--depth-max', '0.495',
+							'--variable', str(var),
+							'--out-dir', outdir+'/cmems/tmp',
+							'--out-name', 'cmems_'+str(var)+'_'+str(day)+'.nc',
+							'--user', username,
+							'--pwd', password],
+							check=True,
+							timeout=300)
+					except subprocess.TimeoutExpired as e:
+						print(var)
+						print(e.stdout)
+						print(e.stderr)
+						continue
+					else:
+						break
+					finally:
+						run += 1
+	ds = xr.open_mfdataset(outdir+'/cmems/tmp/cmems_*.nc', combine='by_coords', decode_times=False)
+	ds.to_netcdf(outdir+'/cmems/cmems.nc')
--- era5/download_era5.py
+++ era5/download_era5.py
@@ -1,71 +1,29 @@
 # coding: utf-8
 """Purpose: download ERA5 data from the Copernicus Data Store
-Dependencies: $HOME/.cdsapirc (for more info: https://cds.climate.copernicus.eu/api-how-to#install-the-cds-api-key)
+
+Dependencies: 
+- $HOME/.cdsapirc (for more info: https://cds.climate.copernicus.eu/api-how-to#install-the-cds-api-key)
+- `conda env create -f environment.yaml`
 
 Creation date: 5 May 2022
 Author: backeb <bjorn.backeberg@deltares.nl> Bjorn Backeberg
 """
 import cdsapi
-import click
 from datetime import datetime, timedelta
 from pathlib import Path
 import xarray as xr
 
-@click.command()
-@click.option(	'--longitude_min',
-		type=(float),
-		help='Minimum longitude for region of interest',
-		default=-180,
-		show_default=True)
-@click.option(	'--longitude_max',
-		type=(float), 
-		help='Maximum longitude for region of interest',
-		default=180,
-		show_default=True)
-@click.option(	'--latitude_min',
-		type=(float),
-		help='Minimum latitude for region of interest',
-		default=-90,
-		show_default=True)
-@click.option(	'--latitude_max',
-		type=(float),
-		help='Maximum latitude for region of interest',
-		default=90,
-		show_default=True)
-@click.option(	'--date_min',
-		type=(str),
-		help='Start date for data download. Format: YYYY-MM-DD',
-		default=(datetime.now()).strftime('%Y-%m-%d'),
-		show_default=True)
-@click.option(	'--date_max',
-		type=(str),
-		help='End date for data download. Format: YYYY-MM-DD',
-		default=(datetime.now()).strftime('%Y-%m-%d'),
-		show_default=True)
-@click.option(	'--vars',
-		multiple=True,
-		help='List of available vars: https://confluence.ecmwf.int/display/CKB/ERA5%3A+data+documentation',
-		default=(	'10m_u_component_of_wind',
-				'10m_v_component_of_wind',
-				'mean_sea_level_pressure',
-				'2m_dewpoint_temperature',
-				'relative_humidity',
-				'surface_net_solar_radiation',
-				'2m_temperature',
-				'total_cloud_cover'),
-		show_default=True)
-
-def download_era5(longitude_min, longitude_max, latitude_min, latitude_max, date_min, date_max, vars):
+def get_era5_single_levels(longitude_min, longitude_max, latitude_min, latitude_max, date_min, date_max, vars, outdir):
 	c = cdsapi.Client()
 	# here we make the strings to use in the api 
 	areastr = [str(longitude_min)+'/'+str(latitude_min)+'/'+str(longitude_max)+'/'+str(latitude_max)]
 	vars = list(vars) # convert tuple to list for cdsapi
 	#make the /data/era5 directory if it does not exist
-	Path('/data/era5').mkdir(parents=True, exist_ok=True)
+	Path(outdir+'/era5/tmp').mkdir(parents=True, exist_ok=True)
 	delta = datetime.strptime(date_max, '%Y-%m-%d') - datetime.strptime(date_min, '%Y-%m-%d')
 	for i in range(delta.days+1):
 		day = datetime.strptime(date_min, '%Y-%m-%d').date() + timedelta(days=i)
-		check_file = Path('/data/era5/era5_'+str(day)+'.nc')
+		check_file = Path(outdir+'/era5/tmp/era5_'+str(day)+'.nc')
 		while not check_file.is_file():
 			yearstr = [f'{day.year:0>4}']
 			monthstr = [f'{day.month:0>2}']
@@ -92,6 +50,5 @@ def download_era5(longitude_min, longitude_max, latitude_min, latitude_max, date
 				'format':'netcdf'
 				},
 				check_file)
-
-if __name__ == '__main__':
-	download_era5()
+	ds = xr.open_mfdataset(outdir+'/era5/tmp/era5_*.nc', combine='by_coords', decode_times=False)
+	ds.to_netcdf(outdir+'/era5/era5.nc')
